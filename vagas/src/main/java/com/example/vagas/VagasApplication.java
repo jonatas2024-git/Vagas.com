02913c4c127eb602000ad46741f5caa900bbb365
Jonatas Pereira <jonatas.pereira@seplag.rj.gov.br>
2025-09-14 00:21:16 -0300

    atualização

--- vagas/src/main/java/com/example/vagas/VagasApplication.java
+++ vagas/src/main/java/com/example/vagas/VagasApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class VagasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VagasApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(VagasApplication.class, args);
+    }
 }
